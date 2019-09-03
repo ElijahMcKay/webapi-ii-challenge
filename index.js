17c882b2e3741451671494038b17dfeef8b22815
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-09-03 17:40:14 -0500

    finished post requests

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ server.use(express.json());
 server.get("/api/posts", (req, res) => {
     users.find()
       .then(users => {
-        res.status(200).json(users);
+        res.status(200).json(posts);
       })
       .catch(() => {
         res.status(500).json({
@@ -24,32 +24,73 @@ server.get("/api/posts", (req, res) => {
       });
   });
 
-  server.get("/api/posts/:id", (req, res) => {
+server.get("/api/posts/:id", (req, res) => {
     users.find()
       .then(users => {
-        res.status(200).json(users);
+        res.status(200).json(posts);
       })
       .catch(() => {
         res.status(500).json({
           message: "Cannot access users data"
         });
       });
-  });
+});
 
-  server.get("/api/posts/:id/comments", (req, res) => {
+server.get("/api/posts/:id/comments", (req, res) => {
     users.find()
       .then(users => {
-        res.status(200).json(users);
+        res.status(200).json(posts);
       })
       .catch(() => {
         res.status(500).json({
           message: "Cannot access users data"
         });
       });
-  });
+});
 
 
 
   // ----- POST Requests -----
+server.post('/api/posts', (req, res) => {
+    
+    users.insert(req.body) 
+        .then(user => {
+            res.status(201).json(posts); 
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "Failed to add new user"
+            }); 
+        })
+})
+
+server.post('/api/posts/:id/comments', (req, res) => {
+    users.insert(req.body) 
+        .then(user => {
+            res.status(201).json(posts); 
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "Failed to add new user"
+            }); 
+        })
+})
+
+
+// ---- PUT Requests ----
+server.put('/api/users/:id', (req, res) => {
+    users.update(req.params.id, req.body)
+        .then(user => {
+            res.status(201).json(posts); 
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "Cannot access users data"
+            });
+        }) 
   
+})
+
+
+// 
 
